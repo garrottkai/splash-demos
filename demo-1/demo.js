9e0e494e0bb86ecf4ddd1cae33f517af2f28bbc6
Kai Garrott <garrottkai@gmail.com>
2017-08-15 14:28:52 -0600

    trying to transform things; add stats

--- demo-1/demo.js
+++ demo-1/demo.js
@@ -12,59 +12,19 @@ var farClippingPlane = 10000;
 var camera = new THREE.PerspectiveCamera( fov, aspect, nearClippingPlane, farClippingPlane );
 
 camera.position.set( 0, 0, 5000 );
-/*
-var geometry = new THREE.SphereGeometry(50, 50, 50);
-
-//var loader = new THREE.TextureLoader();
-//loader.crossOrigin = 'anonymous';
-//var texture = loader.load('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/textures/crate.gif');
-
-var material = new THREE.LineBasicMaterial( {
-color: 0xff4400,
-} );
-
-var mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-var ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
-scene.add(ambientLight);
-
-var directionalLight = new THREE.DirectionalLight(0xffffff, 2.0);
-directionalLight.position.set(0, 10, 0);
-scene.add(directionalLight);
-*/
-/*
-window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize() {
-camera.aspect = window.innerWidth / window.innerHeight;
-camera.updateProjectionMatrix();
-renderer.setSize( window.innerWidth, window.innerHeight );
-}
-*/
 
 
-
-var particles = 50000;
+var stats = new Stats();
+document.body.appendChild( stats.dom );
+var particles = 10000;
 var positions = new Float32Array(particles * 3);
 var geometry = new THREE.BufferGeometry();
 
 var colors = new Float32Array( particles * 3 );
 
 var color = new THREE.Color();
-//var rad = geometry.radius;
-/*
-function getRandomInt(min, max) {
-min = Math.ceil(min);
-max = Math.floor(max);
-return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-*/
+
 for ( var i = 0; i < positions.length; i += 3 ) {
-
-
-
-    // positions
 
     var randomDirection = new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
     var magnitudeFactor = Math.random() * 500;
@@ -73,8 +33,6 @@ for ( var i = 0; i < positions.length; i += 3 ) {
     positions[ i ]     = randomDirection.x;
     positions[ i + 1 ] = randomDirection.y;
     positions[ i + 2 ] = randomDirection.z;
-
-    // colors
 
     var vx = 255;
     var vy = 69;
@@ -98,27 +56,33 @@ var material = new THREE.PointsMaterial({
     color: 0xff4400,
     size: 10,
     map: particleMap,
+    blending: THREE.AdditiveBlending,
     transparent: true
 });
 
 particleSystem = new THREE.Points( geometry, material );
 scene.add( particleSystem );
 
-yRotationSpeed = 0.2;
+yRotationSpeed = 0.1;
 function animate() {
     requestAnimationFrame(animate);
 
     //particleSystem.rotation.x += 0.01;
     particleSystem.rotation.y += yRotationSpeed;
-    particleSystem.rotation.z += 0.01;
+    //particleSystem.rotation.z += 0.01;
     if(camera.position.z >= 500) {
         camera.position.z -= 10;
         if(yRotationSpeed > 0.01) {
-        yRotationSpeed -= 0.0001;
+            yRotationSpeed -= 0.00015;
+        }
     }
+
+    if (camera.position.z === 3000) {
+        // need to transform everything here
     }
 
     renderer.render(scene, camera);
+    stats.update();
 }
 
 animate();
