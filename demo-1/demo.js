d28d547539722450cd00aecb15a89085f3312df0
Kai Garrott <garrottkai@gmail.com>
2017-08-17 15:13:52 -0600

    minor cleanup; change lighting color

--- demo-1/demo.js
+++ demo-1/demo.js
@@ -1,4 +1,5 @@
-// <spaghetti type="revolting">
+// author: Kai Garrott <kai@lyradevelopment.com>
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('container').insertBefore(renderer.domElement, document.getElementById('overlay'));
@@ -10,7 +11,6 @@ var fov = 75;
 var aspect = window.innerWidth / window.innerHeight;
 var nearClippingPlane = 0.1;
 var farClippingPlane = 5000;
-
 var camera = new THREE.PerspectiveCamera( fov, aspect, nearClippingPlane, farClippingPlane );
 camera.position.set( 0, 0, 3000 );
 
@@ -60,23 +60,7 @@ function onWindowResize() {
 var backdrop;
 var yRotationSpeed = -0.3;
 var approachSpeed = 17;
-function wat() {
-    var vecs = geometry.attributes.position.array;
-    /*for(var i = 0; i < vecs.length; i += 3) {
-        vecs[i] *= 1.1;
-        vecs[i + 1] *= 1.1;
-        vecs[i + 2] *= 1.1;
-        //var r = Math.random() * 0.1 + 1.06;
-        //vecs[i] *= r;
-        //vecs[i + 1] *= r;
-        //vecs[i + 2] *= r;
-    }*/
-    for(var i = 0; i < vecs.length; i ++) {
-        vecs[i] *= 1.1;
-    }
-    yRotationSpeed *= .63;
-    geometry.attributes.position.needsUpdate = true;
-}
+
 function animate() {
     requestAnimationFrame(animate);
 
@@ -88,14 +72,28 @@ function animate() {
 
     }
     if (camera.position.z >= 1700 && camera.position.z <= 1900) {
-        setTimeout(wat, 0);
+        var vecs = geometry.attributes.position.array;
+        /*for(var i = 0; i < vecs.length; i += 3) {
+            vecs[i] *= 1.1;
+            vecs[i + 1] *= 1.1;
+            vecs[i + 2] *= 1.1;
+            //var r = Math.random() * 0.1 + 1.06;
+            //vecs[i] *= r;
+            //vecs[i + 1] *= r;
+            //vecs[i + 2] *= r;
+        }*/
+        for(var i = 0; i < vecs.length; i ++) {
+            vecs[i] *= 1.1;
+        }
+        yRotationSpeed *= .63;
+        geometry.attributes.position.needsUpdate = true;
     }
 
     if(camera.position.z <= 1700 && !backdrop) {
         var backdropMaterial = new THREE.MeshLambertMaterial({color: 0x555555});
         backdrop = new THREE.Mesh(new THREE.PlaneGeometry(9000, 5000), backdropMaterial);
         backdrop.position.z = -2000;
-        var lighting = new THREE.PointLight(0x86fdff/*0xA0E7FF*/, 0.1, 0);
+        var lighting = new THREE.PointLight(0x86fdff/*0x89E3FF*/, 0.1, 0);
         lighting.name = 'lighting';
         scene.add(backdrop, lighting);
     }
@@ -122,4 +120,3 @@ function animate() {
 }
 
 animate();
-// </spaghetti>
