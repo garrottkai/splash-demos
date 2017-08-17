c27ce5ee56afe6e9d9c7fe2159dc8937639ccb6c
Kai Garrott <garrottkai@gmail.com>
2017-08-17 10:20:01 -0600

    bye, Math.random()

--- demo-1/demo.js
+++ demo-1/demo.js
@@ -21,12 +21,7 @@ document.body.appendChild( stats.dom );
 var particles = 60000;
 var positions = new Float32Array(particles * 3);
 var geometry = new THREE.BufferGeometry();
-geometry.dynamic = true;
-/*
-var colors = new Float32Array( particles * 3 );
 
-var color = new THREE.Color();
-*/
 for ( var i = 0; i < positions.length; i += 3 ) {
 
     var randomDirection = new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
@@ -36,21 +31,10 @@ for ( var i = 0; i < positions.length; i += 3 ) {
     positions[ i ]     = randomDirection.x;
     positions[ i + 1 ] = randomDirection.y;
     positions[ i + 2 ] = randomDirection.z;
-    /*
-    var vx = 255;
-    var vy = 69;
-    var vz = 0;
 
-    color.setRGB(0xff4500);
-
-    colors[ i ]     = color.r;
-    colors[ i + 1 ] = color.g;
-    colors[ i + 2 ] = color.b;
-    */
 }
 
 geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-//geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
 
 var loader = new THREE.TextureLoader();
 var particleMap = loader.load('circle.png');
@@ -80,26 +64,23 @@ var approachSpeed = 17
 function animate() {
     requestAnimationFrame(animate);
 
-    //particleSystem.rotation.x += 0.01;
     particleSystem.rotation.z += yRotationSpeed;
     particleSystem.rotation.y += yRotationSpeed;
-    //particleSystem.rotation.z += 0.01;
+
     if(camera.position.z >= 800) {
         camera.position.z -= approachSpeed;
-        //    if(yRotationSpeed > 0.013) {
-        //        yRotationSpeed -= 0.001;
-        //    }
+
     }
     if (camera.position.z >= 1700 && camera.position.z <= 1900) {
         var vecs = geometry.attributes.position.array;
         for(var i = 0; i < vecs.length; i += 3) {
-            //vecs[i] *= 1.2;
-            //vecs[i + 1] *= 1.2
-            //vecs[i + 2] *= 1.2;
-            var r = Math.random() * 0.1 + 1.06;
-            vecs[i] *= r;
-            vecs[i + 1] *= r;
-            vecs[i + 2] *= r;
+            vecs[i] *= 1.1;
+            vecs[i + 1] *= 1.1;
+            vecs[i + 2] *= 1.1;
+            //var r = Math.random() * 0.1 + 1.06;
+            //vecs[i] *= r;
+            //vecs[i + 1] *= r;
+            //vecs[i + 2] *= r;
         }
         yRotationSpeed *= .63;
         geometry.attributes.position.array = vecs;
